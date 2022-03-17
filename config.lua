59fc328be7770428138f5ebb7a92a4d33e0827c6
TweakyGaming <tweakymedia@gmail.com>
2022-03-17 22:53:13 +0100

    blib deactivated

--- config.lua
+++ config.lua
@@ -4,81 +4,81 @@ Config.DrawDistance = 10.0
 Config.MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 
 Config.Money = 50 -- How much it takes from you when you open the clothing menu
-Config.Debug = true
+Config.Debug = false
 
 Config.ClothingShops = {
 	[1] = {
-		blip = true,
+		blip = false,
 		coords = vector3(72.3, -1399.1, 28.4),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[2] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-708.71, -152.13, 36.4),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[3] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-165.15, -302.49, 38.6),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[4] = {
-		blip = true,
+		blip = false,
 		coords = vector3(428.7, -800.1, 28.5),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[5] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-829.4, -1073.7, 10.3),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[6] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-1449.16, -238.35, 48.8),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[7] = {
-		blip = true,
+		blip = false,
 		coords = vector3(11.6, 6514.2, 30.9),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[8] = {
-		blip = true,
+		blip = false,
 		coords = vector3(122.98, -222.27, 53.5),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[9] = {
-		blip = true,
+		blip = false,
 		coords = vector3(1696.3, 4829.3, 41.1),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[10] = {
-		blip = true,
+		blip = false,
 		coords = vector3(618.1, 2759.6, 41.1),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[11] = {
-		blip = true,
+		blip = false,
 		coords = vector3(1190.6, 2713.4, 37.2),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[12] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-1193.4, -772.3, 16.3),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[13] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-3172.5, 1048.1, 19.9),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[14] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-1108.4, 2708.9, 18.1),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
 	[15] = {
-		blip = true,
+		blip = false,
 		coords = vector3(-1169.88, -1580.88, 4.38),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},
@@ -89,11 +89,11 @@ Config.ClothingShops = {
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
 	},	
 	-- EMS
-	--[[[17] = {
-		blip = true,
+	[17] = {
+		blip = false,
 		coords = vector3(300.60162353516, -597.76068115234, 42.18409576416),
 		MarkerSize   = {x = 6.0, y = 6.0, z = 1.0}
-	},--]]
+	},
 }
 
 Config.BarberShops = {
